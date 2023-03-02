65c0a9cb485e12f12a6a6aa84f43e00ff840e742
Daniyar <baddaniar@gmail.com>
2023-03-02 18:38:37 +0600

    hw5 done without styles

---
+++ homework5/src/components/TodoList/TodoList.tsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import apiClient from "../../common/api";
+import { ITodos } from "../../types";
+import Todo from "../Todo/Todo";
+
+
+const PostList = () => {
+  const [todos, setTodos] = useState<ITodos[]>([]);
+  const [isLoading, setIsLoading] = useState(true)
+
+  const getPost = async () => {
+    try {
+      const res = await apiClient.get<ITodos[]>("/todos");
+      setTodos(res.data);
+    } catch (error) {
+      console.log({ error });
+    } finally{
+        setIsLoading(false)
+    }
+  };
+
+  //mounted
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  if(isLoading){
+    return(
+        <div>Loading</div>
+    )
+  }
+
+  return (
+    <div>
+      {todos.map((todo) => <Todo key={todo.id} {...todo}/>)}
+    </div>
+  );
+};
+
+export default PostList;
